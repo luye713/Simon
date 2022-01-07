d4f75d853903447bb9d8d2d310defc10949ee4fc
Lu Ye <alison94713@gmail.com>
2022-01-07 00:55:35 -0500

    update functions and styles.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 /*----- constants -----*/
-const MAX_LEVEL = 3;
+const MAX_LEVEL = 10;
 
 /*----- app's state (variables) -----*/
 let currentLevel = 0;
@@ -7,118 +7,143 @@ let gamePat = [];
 let playerPat = [];
 
 /*----- cached element references -----*/
+const boardEl = document.getElementById("game_board");
 const crystals = document.querySelectorAll(".crystal");
-const buttonEl = document.querySelector("button");
 const levelEl = document.getElementById("level_num");
 const msgEl = document.getElementById("message");
-const bodyEl = document.querySelector("body");
+const buttonEl = document.querySelector("button");
 
 /*----- event listeners -----*/
 buttonEl.addEventListener("click", handleBtnClick);
 
-crystals.forEach(function(crystal){
-    crystal.addEventListener("click", handleCrystalClick)
+crystals.forEach(function(crystal) {
+    crystal.addEventListener("click", handleCrystalClick);
 });
-
 
 /*----- functions -----*/
 
-//to start/restart the game at level one.
+//to start or retry the game
 function handleBtnClick() {
-    bodyEl.style.pointerEvents = "none";
+    boardEl.style.pointerEvents = "none";
     buttonEl.style.display = "none";
-    msgEl.textContent = "game start";
     currentLevel = 1;
+    getLevelPat();
+};
+
+//to generate game pattern for each level
+function getLevelPat() {
+    msgEl.textContent = "";
     levelEl.textContent = currentLevel;
-    getRandomCrystal();
-    gamePat.push(randomCrystal);
-    console.log(gamePat)
+
+    for(let i = 0; i < currentLevel; i++) {
+        getRandomCrystal();
+        gamePat.push(randomCrystal);
+    };
+
     lightCrystal();
-}
+};
 
 //to generate a random crystal.
 function getRandomCrystal() {
-    let randomNum = Math.floor(Math.random() * 5);
-    randomCrystal = crystals[randomNum].getAttribute("id");
+    let randomIdx = Math.floor(Math.random() * 5);
+    randomCrystal = crystals[randomIdx].getAttribute("id");
     return randomCrystal;
-}
+};
+
+//to light up the crystals 
+function lightCrystal() {
+    gamePat.forEach(function(color, idx) {
+        let activeCrystal = document.getElementById(color);
+
+        setTimeout(function() {
+            activeCrystal.style.opacity = "1";
+            activeCrystal.style.transform = "scale(0.95)";
+            activeCrystal.childNodes[0].play();
+
+            setTimeout(function() {
+                activeCrystal.style.opacity = "0.6";
+                activeCrystal.style.transform = "scale(1)";
+            },300);
+
+        }, (idx+1) * 1000 + 500);
+    });
+    
+    setTimeout(function(){
+        boardEl.style.pointerEvents = "auto";
+        msgEl.textContent = "YOUR TURN";
+    }, gamePat.length * 1000 + 700);
+};
 
 //to save player's input into the playerPat array.
-//then validate player's choices.
 function handleCrystalClick(evt) {
     let clickedCrystal = evt.target.getAttribute("id");
     playerPat.push(clickedCrystal);
-    console.log(playerPat)
-    evt.target.style.opacity = "1"
+
+    evt.target.childNodes[0].play();
+    evt.target.style.opacity = "1";
+    evt.target.style.transform = "scale(0.95)";
+    
     setTimeout(function(){
-        evt.target.style.opacity = "0.6"
-    }, 500)
+        evt.target.style.opacity = "0.6";
+        evt.target.style.transform = "scale(1)";
+    }, 300);
     
     playerPat.forEach(checkPlayerPat);
 }
 
 
-
-
 //to compare playerPat with gamePat.
-//level 2: gamePat = [purple, green]
-//palyerPat = [purple]
-function checkPlayerPat(color, idx){
-    if (color === gamePat[idx]){
-        console.log(gamePat.length)
-        console.log(playerPat.length)
-        console.log(idx)
+function checkPlayerPat(color, idx) {
+    if (color === gamePat[idx]) {
         if (idx+1 === gamePat.length) {
-            
-            if (currentLevel < MAX_LEVEL){
-                nextLevel()
-                console.log(gamePat)
+            if (currentLevel < MAX_LEVEL) {
+                nextLevel();
             } else {
-                msgEl.textContent = "YOU ARE THE WINNER!!!";
+                winEffects();
+                msgEl.textContent = "YOU WIN!";
+                buttonEl.textContent = "START"
+                document.getElementById("win").play();
+                boardEl.style.pointerEvents = "none";
                 buttonEl.style.display = "block";
-                gamePat = [];
-                playerPat = [];
+                patReset();
             }
-        }else{console.log("hah")}
+        }
     } else {
-        msgEl.textContent = "you lose, start over";
-        buttonEl.style.display  = "block";
-        gamePat = [];
-        playerPat = [];
+        setTimeout(function() {
+            document.getElementById("lose").play();
+            msgEl.textContent = "YOU LOSE";
+            buttonEl.style.display  = "block";
+            buttonEl.textContent = "RETRY";
+        }, 500);
+        patReset();
     }
 }
 
 //to generate a new gamePat array for the next level
 function nextLevel() {
-    bodyEl.style.pointerEvents = "none";
+    boardEl.style.pointerEvents = "none";
     currentLevel++;
-    levelEl.textContent = currentLevel;
-    gamePat = [];
-    playerPat = [];
-    for(let i = 0; i < currentLevel; i++) {
-        getRandomCrystal()
-        gamePat.push(randomCrystal);
-    }
-    lightCrystal();
-    msgEl.textContent = "AI";
+    patReset();
+    getLevelPat();
 }
 
-//to light up the crystals 
-//level 3: gamePat = [blue, green, yellow]
-//index: 0, 1, 2
-//timer: 1s, 2s, 3s from now
-function lightCrystal() {
-    gamePat.forEach(function(color, idx) {
-        let activeCrystal = document.getElementById(color)
+//special effect for winner
+function winEffects() {
+    crystals.forEach(function(crystal, idx) {
+
         setTimeout(function() {
-            activeCrystal.style.opacity = "1";
+            crystals[idx].style.opacity = "1";
+
             setTimeout(function() {
-                activeCrystal.style.opacity = "0.6";
-            },500)
-        }, (idx+1) * 1500)
-    })
-    setTimeout(function(){
-        bodyEl.style.pointerEvents = "all";
-        msgEl.textContent = "your turn";
-    }, gamePat.length * 1500 + 500)
-}
+                crystals[idx].style.opacity = "0.6";
+            }, 400);
+
+        }, (idx+1) * 300);
+    });
+};
+
+//to reset game and player pattern
+function patReset() {
+    gamePat = [];
+    playerPat = [];
+};
